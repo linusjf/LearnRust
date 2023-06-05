22b748f9c1b05feac32f7f65e769b46b71e66681
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-05 21:05:37 +0530

    feat: add more println statements

--- comprehensiverust/exercise/src/main.rs
+++ comprehensiverust/exercise/src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
     println!("Edit me!");
     println!("Hello, 🌍!");
+    println!(r#"<a href="link.html">link</a>"#);
+    println!("<a href=\"link.html\">link</a>");
+    println!("{:?}", b"abc");
+    println!("{:?}", &[97, 98, 99]);
 }
