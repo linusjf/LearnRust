0df7127484edbd8072016d6166b06b68b741b495
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-05 21:43:29 +0530

    feat: add more println statements

--- comprehensiverust/exercise/src/main.rs
+++ comprehensiverust/exercise/src/main.rs
@@ -5,4 +5,13 @@ fn main() {
     println!("<a href=\"link.html\">link</a>");
     println!("{:?}", b"abc");
     println!("{:?}", &[97, 98, 99]);
+    let mut a: [i8; 10] = [42; 10];
+    a[5] = 0;
+    println!("a: {a:?}");
+    println!("a: {a:#?}");
+    println!("a: {:?}", a);
+    println!("a: {:#?}", a);
+    let t: (i8, bool) = (7, true);
+    println!("1st index: {}", t.0);
+    println!("2nd index: {}", t.1);
 }
