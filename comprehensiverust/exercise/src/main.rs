64909c5328d41ec5d2b69ab44922361f9eba2033
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-06 17:37:57 +0530

    feat: add fizzbuzz example

--- comprehensiverust/exercise/src/main.rs
+++ comprehensiverust/exercise/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     tuples();
     references();
     strings();
+    print_fizzbuzz_to(100);
 }
 
 fn hello() {
@@ -73,4 +74,27 @@ fn strings() {
     println!("s2: {s2}");
     let s3: &str = &s2[7..];
     println!("s3: {s3}");
+}
+
+fn is_divisible(n: u32, divisor: u32) -> bool {
+    if divisor == 0 {
+        return false;
+    }
+    n % divisor == 0
+}
+
+fn fizzbuzz(n: u32) -> String {
+    let fizz = if is_divisible(n, 3) { "fizz" } else { "" };
+    let buzz = if is_divisible(n, 5) { "buzz" } else { "" };
+    if fizz.is_empty() && buzz.is_empty() {
+        return format!("{n}");
+    }
+    format!("{fizz}{buzz}")
+}
+
+fn print_fizzbuzz_to(n: u32) {
+    for i in 1..=n {
+        print!(" {} ", fizzbuzz(i));
+    }
+    println!("")
 }
