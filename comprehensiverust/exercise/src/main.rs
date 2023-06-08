23c4ec5ce7b137d8944210893063e1f1582bfd3f
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-08 14:23:59 +0530

    feat: add transpose example

--- comprehensiverust/exercise/src/main.rs
+++ comprehensiverust/exercise/src/main.rs
@@ -11,6 +11,7 @@ fn main() {
     generics();
     conversions();
     forloops();
+    printtranspose();
 }
 
 fn hello() {
@@ -191,4 +192,35 @@ fn forloops() {
         print!(" {}", array[i]);
     }
     println!();
+}
+
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    let mut result = [[0; 3]; 3];
+    for i in 0..3 {
+        for j in 0..3 {
+            result[j][i] = matrix[i][j];
+        }
+    }
+    return result;
+}
+
+fn pretty_print(matrix: &[[i32; 3]; 3]) {
+    for row in matrix {
+        println!("{row:?}");
+    }
+}
+
+fn printtranspose() {
+    let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    println!("matrix:");
+    pretty_print(&matrix);
+
+    let transposed = transpose(matrix);
+    println!("transposed:");
+    pretty_print(&transposed);
 }
