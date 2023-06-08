eb4ad4655c91ef9a5b40a8516b80aa1c123bd88a
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-08 20:20:34 +0530

    feat: add borrowing example

--- comprehensiverust/exercise/src/main.rs
+++ comprehensiverust/exercise/src/main.rs
@@ -20,6 +20,8 @@ fn main() {
     stackmemory();
     movesemantics();
     copyclone();
+    borrowing();
+    stackreturns();
 }
 
 fn hello() {
@@ -364,4 +366,25 @@ fn copyclone() {
     let p2 = p1.clone();
     println!("p1: {p1:?}");
     println!("p2: {p2:?}");
+}
+
+fn add(p1: &Point, p2: &Point) -> Point {
+    let p = Point(p1.0 + p2.0, p1.1 + p2.1);
+    println!("&p.0: {:p}", &p.0);
+    p
+}
+
+fn borrowing() {
+    let p1 = Point(3, 4);
+    let p2 = Point(10, 20);
+    let p3 = add(&p1, &p2);
+    println!("{p1:?} + {p2:?} = {p3:?}");
+}
+
+fn stackreturns() {
+    let p1 = Point(3, 4);
+    let p2 = Point(10, 20);
+    let p3 = add(&p1, &p2);
+    println!("&p3.0: {:p}", &p3.0);
+    println!("{p1:?} + {p2:?} = {p3:?}");
 }
