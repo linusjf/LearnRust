b4bad2b1b51809c1c8aec90beb9c3221547c2f68
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-10 13:38:55 +0530

    feat: add structs example

--- comprehensiverust/exercise/src/structs.rs
+++ comprehensiverust/exercise/src/structs.rs
@@ -1,6 +1,48 @@
+#[derive(Debug)]
 struct Person {
     name: String,
     age: u8,
+}
+impl Person {
+    fn new(name: String, age: u8) -> Person {
+        Self { name, age }
+    }
+}
+
+impl Default for Person {
+    fn default() -> Person {
+        Person {
+            name: "Bot".to_string(),
+            age: 0,
+        }
+    }
+}
+
+fn create_default() -> Person {
+    let tmp = Person {
+        ..Default::default()
+    };
+    println!("{:#?}", tmp);
+    let tmp = Person {
+        name: "Sam".to_string(),
+        ..Default::default()
+    };
+    println!("{:#?}", tmp);
+    tmp
+}
+
+struct Point(i32, i32);
+
+struct PoundsOfForce(f64);
+struct Newtons(f64);
+
+fn compute_thruster_force() -> PoundsOfForce {
+    // todo!("Ask a rocket scientist at NASA")
+    return PoundsOfForce(64.0);
+}
+
+fn set_thruster_force(_force: PoundsOfForce) {
+    // ...
 }
 
 pub fn main() {
@@ -18,4 +60,11 @@ pub fn main() {
         ..peter
     };
     println!("{} is {} years old", jackie.name, jackie.age);
+    let p = Point(17, 23);
+    println!("({}, {})", p.0, p.1);
+    let force = compute_thruster_force();
+    set_thruster_force(force);
+    let peter = Person::new(String::from("Peter"), 27);
+    println!("{peter:?}");
+    create_default();
 }
