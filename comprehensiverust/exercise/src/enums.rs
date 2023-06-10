9cbf92b5636ded65c4b7d0575dd014782db6e8b8
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-10 16:29:27 +0530

    feat: add enums example

--- comprehensiverust/exercise/src/enums.rs
+++ comprehensiverust/exercise/src/enums.rs
@@ -1,3 +1,4 @@
+#![recursion_limit = "1000"]
 use std::mem;
 use std::mem::transmute;
 use std::mem::{align_of, size_of};
@@ -118,4 +119,48 @@ pub fn main() {
     dbg_size!(&i32);
     dbg_size!(Option<&i32>);
     bits();
+    options();
+}
+
+macro_rules! dbg_bits {
+    ($e:expr, $bit_type:ty) => {
+        println!("- {}: {:#x}", stringify!($e), transmute::<_, $bit_type>($e));
+    };
+}
+
+// Macro to wrap a value in 2^n Some() where n is the number of "@" signs.
+// Increasing the recursion limit is required to evaluate this macro.
+macro_rules! many_options {
+    ($value:expr) => {
+        Some($value)
+    };
+    ($value:expr, @) => {
+        Some(Some($value))
+    };
+    ($value:expr, @ $($more:tt)+) => {
+        // many_options!(many_options!($value, $($more)+), $($more)+)
+    };
+}
+
+fn options() {
+    // TOTALLY UNSAFE. Rust provides no guarantees about the bitwise
+    // representation of types.
+    unsafe {
+        assert_eq!(many_options!(false), Some(false));
+        assert_eq!(many_options!(false, @), Some(Some(false)));
+        //    assert_eq!(many_options!(false, @@), Some(Some(Some(Some(false)))));
+
+        println!("Bitwise representation of a chain of 128 Option's.");
+        //        dbg_bits!(many_options!(false, @@@@@@@), u8);
+        //      dbg_bits!(many_options!(true, @@@@@@@), u8);
+
+        println!("Bitwise representation of a chain of 256 Option's.");
+        //    dbg_bits!(many_options!(false, @@@@@@@@), u16);
+        //  dbg_bits!(many_options!(true, @@@@@@@@), u16);
+
+        println!("Bitwise representation of a chain of 257 Option's.");
+        //dbg_bits!(many_options!(Some(false), @@@@@@@@), u16);
+        // dbg_bits!(many_options!(Some(true), @@@@@@@@), u16);
+        // dbg_bits!(many_options!(None::<bool>, @@@@@@@@), u16);
+    }
 }
