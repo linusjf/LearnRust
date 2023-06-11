d6babbd1cd3244c3b3f30347ad1ca34dafc9abfe
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-11 05:45:51 +0530

    feat: add patterns example

--- comprehensiverust/exercise/src/patterns.rs
+++ comprehensiverust/exercise/src/patterns.rs
@@ -60,4 +60,16 @@ fn destructs() {
             _ => println!("All elements were ignored"),
         }
     }
+    inspect(&[0, -2, 3]);
+    inspect(&[0, -2, 3, 4]);
+    inspect(&[1, -2, 3, 4, 5]);
+}
+
+fn inspect(slice: &[i32]) {
+    println!("Tell me about {slice:?}");
+    match slice {
+        &[0, y, z] => println!("First is 0, y = {y}, and z = {z}"),
+        &[1, ..] => println!("First is 1 and the rest were ignored"),
+        _ => println!("All elements were ignored"),
+    }
 }
