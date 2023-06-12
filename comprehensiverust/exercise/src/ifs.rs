427d9689cae2c593d6c1cd851be29768d8ff5156
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-12 10:17:48 +0530

    feat: add conditionals example

--- comprehensiverust/exercise/src/ifs.rs
+++ comprehensiverust/exercise/src/ifs.rs
@@ -42,6 +42,24 @@ pub fn main() {
     for x in v.iter_mut() {
         println!("x: {x}");
     }
+    let mut x = 10;
+    loop {
+        x = if x % 2 == 0 { x / 2 } else { 3 * x + 1 };
+        if x == 1 {
+            break;
+        }
+    }
+    println!("Final x: {x}");
+    let binding = std::env::args().next();
+    let x = binding.as_deref();
+    match x {
+        Some("cat") => println!("Will do cat things"),
+        Some("ls") => println!("Will ls some files"),
+        Some("mv") => println!("Let's move some files"),
+        Some("rm") => println!("Uh, dangerous!"),
+        None => println!("Hmm, no program name?"),
+        _ => println!("Unknown program name: {:#?}", x),
+    }
 }
 
 fn second_word_to_upper(s: &str) -> Option<String> {
