63f037ea51430273d3a61afb1b4d8a7512cbaf86
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-12 15:14:38 +0530

    feat: add hashmaps example

--- comprehensiverust/stdlib/src/main.rs
+++ comprehensiverust/stdlib/src/main.rs
@@ -1,7 +1,10 @@
+use std::collections::HashMap;
+
 fn main() {
     options();
     strings();
     vectors();
+    hashmaps();
 }
 
 fn options() {
@@ -67,4 +70,33 @@ fn vectors() {
         print!("{:#?} ", v3.get(i));
     }
     println!();
+}
+
+fn hashmaps() {
+    let mut page_counts = HashMap::new();
+    page_counts.insert("Adventures of Huckleberry Finn".to_string(), 207);
+    page_counts.insert("Grimms' Fairy Tales".to_string(), 751);
+    page_counts.insert("Pride and Prejudice".to_string(), 303);
+
+    if !page_counts.contains_key("Les Misérables") {
+        println!(
+            "We know about {} books, but not Les Misérables.",
+            page_counts.len()
+        );
+    }
+
+    for book in ["Pride and Prejudice", "Alice's Adventure in Wonderland"] {
+        match page_counts.get(book) {
+            Some(count) => println!("{book}: {count} pages"),
+            None => println!("{book} is unknown."),
+        }
+    }
+
+    // Use the .entry() method to insert a value if nothing is found.
+    for book in ["Pride and Prejudice", "Alice's Adventure in Wonderland"] {
+        let page_count: &mut i32 = page_counts.entry(book.to_string()).or_insert(0);
+        *page_count += 1;
+    }
+
+    println!("{page_counts:#?}");
 }
