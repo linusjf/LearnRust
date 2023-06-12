0f83b0a9b76bbff5007949ac102e40f1dc2ec68e
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-12 11:08:19 +0530

    feat: add blocks example

--- comprehensiverust/exercise/src/ifs.rs
+++ comprehensiverust/exercise/src/ifs.rs
@@ -60,6 +60,19 @@ pub fn main() {
         None => println!("Hmm, no program name?"),
         _ => println!("Unknown program name: {:#?}", x),
     }
+    let v = vec![10, 20, 30];
+    let mut iter = v.into_iter();
+    'outer: while let Some(x) = iter.next() {
+        println!("x: {x}");
+        let mut i = 0;
+        while i < x {
+            println!("x: {x}, i: {i}");
+            i += 1;
+            if i == 3 {
+                break 'outer;
+            }
+        }
+    }
 }
 
 fn second_word_to_upper(s: &str) -> Option<String> {
