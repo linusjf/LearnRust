c67d92eaefa62fa26f6881113fcaff0b2beacde5
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-13 12:39:25 +0530

    feat: add generics example

--- comprehensiverust/generics/src/main.rs
+++ comprehensiverust/generics/src/main.rs
@@ -5,6 +5,20 @@ struct Point<T> {
 }
 
 #[derive(Debug)]
+struct PointG<T>(T, T);
+
+impl<T> PointG<T> {
+    fn x(&self) -> &T {
+        &self.0 // + 10
+    }
+    fn y(&self) -> &T {
+        &self.1 // + 10
+    }
+
+    // fn set_x(&mut self, x: T)
+}
+
+#[derive(Debug)]
 struct Point2D<T, U> {
     x: T,
     y: U,
@@ -19,4 +33,7 @@ fn main() {
     let float = Point2D { x: 1, y: 4.0 };
     println!("{integer:?} and {float:?}");
     println!("{0:?} and {1:?}", integer.x, float.y);
+    let p = PointG(5, 10);
+    println!("p.x = {}", p.x());
+    println!("p.y = {}", p.y());
 }
