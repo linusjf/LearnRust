9a888805344bb6d41754cf8021f6714b38f1af7a
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-13 13:29:30 +0530

    feat: add more generics example

--- comprehensiverust/generics/src/main.rs
+++ comprehensiverust/generics/src/main.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 #[derive(Debug)]
 struct Point<T> {
     x: T,
@@ -36,4 +37,25 @@ fn main() {
     let p = PointG(5, 10);
     println!("p.x = {}", p.x());
     println!("p.y = {}", p.y());
+    let integer = Some(5);
+    let float = Some(5.0);
+    println!("{integer:?} and {float:?}");
+    let integer = OptionI32::Some(5);
+    let float = OptionF64::Some(5.0);
+    println!("{integer:?} and {float:?}");
+    let integer = OptionI32::None;
+    let float = OptionF64::None;
+    println!("{integer:?} and {float:?}");
+}
+
+#[derive(Debug)]
+enum OptionF64 {
+    Some(f64),
+    None,
+}
+
+#[derive(Debug)]
+enum OptionI32 {
+    Some(i32),
+    None,
 }
