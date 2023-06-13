883bb4a9d79a243e5df90bb53ef9190bb8805adf
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-13 16:07:10 +0530

    feat: add traits example

--- comprehensiverust/traits/src/main.rs
+++ comprehensiverust/traits/src/main.rs
@@ -61,6 +61,10 @@ fn main() {
         &p2,
         if p1 == p2 { "yes" } else { "no" }
     );
+    let a = Centimeter(10);
+    let b = Centimeter(20);
+    println!("{a:?} equals {b:?}: {}", a.equal(&b));
+    println!("{a:?} not_equals {b:?}: {}", a.not_equal(&b));
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Default)]
@@ -68,4 +72,20 @@ struct Player {
     name: String,
     strength: u8,
     hit_points: u8,
+}
+
+trait Equals {
+    fn equal(&self, other: &Self) -> bool;
+    fn not_equal(&self, other: &Self) -> bool {
+        !self.equal(other)
+    }
+}
+
+#[derive(Debug)]
+struct Centimeter(i16);
+
+impl Equals for Centimeter {
+    fn equal(&self, other: &Centimeter) -> bool {
+        self.0 == other.0
+    }
 }
