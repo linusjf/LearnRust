e275d3cf44b0581823a2a19f9085d09ce944bce1
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-16 17:02:02 +0530

    feat: add errorhandling example

--- comprehensiverust/errorhandling/src/main.rs
+++ comprehensiverust/errorhandling/src/main.rs
@@ -1,5 +1,19 @@
+use std::fs::File;
+use std::io::Read;
 use std::panic;
+
 fn main() {
+    let file = File::open("diary.txt");
+    match file {
+        Ok(mut file) => {
+            let mut contents = String::new();
+            let _res = file.read_to_string(&mut contents);
+            println!("Dear diary: {contents}");
+        }
+        Err(err) => {
+            println!("The diary could not be opened: {err}");
+        }
+    }
     let result = panic::catch_unwind(|| {
         println!("hello!");
     });
