679f712ee92d124f0f5f18745592a9104904ecbc
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-17 21:01:32 +0530

    feat: add more unsafe examples

--- comprehensiverust/unsaferust/src/main.rs
+++ comprehensiverust/unsaferust/src/main.rs
@@ -15,4 +15,47 @@ fn main() {
         *r1 = 10;
         println!("r2 is: {}", *r2);
     }
+    println!("HELLO_WORLD: {HELLO_WORLD}");
+    add_to_counter(42);
+    unsafe {
+        println!("COUNTER: {COUNTER}");
+    } // Potential data race!
+    let u = MyUnion { i: 42 };
+    println!("int: {}", unsafe { u.i });
+    println!("bool: {}", unsafe { u.b }); // Undefined behavior!
+    let emojis = "🗻∈🌏";
+    // Safe because the indices are in the correct order, within the bounds of
+    // the string slice, and lie on UTF-8 sequence boundaries.
+    unsafe {
+        println!("emoji: {}", emojis.get_unchecked(0..4));
+        println!("emoji: {}", emojis.get_unchecked(4..7));
+        println!("emoji: {}", emojis.get_unchecked(7..11));
+    }
+    println!(
+        "char count: {}",
+        count_chars(unsafe { emojis.get_unchecked(0..7) })
+    );
+    // Not upholding the UTF-8 encoding requirement breaks memory safety!
+    // println!("emoji: {}", unsafe { emojis.get_unchecked(0..3) });
+    // println!("char count: {}", count_chars(unsafe { emojis.get_unchecked(0..3) }));
+}
+
+static HELLO_WORLD: &str = "Hello, world!";
+
+static mut COUNTER: u32 = 0;
+
+fn add_to_counter(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    } // Potential data race!
+}
+
+#[repr(C)]
+union MyUnion {
+    i: u8,
+    b: bool,
+}
+
+fn count_chars(s: &str) -> usize {
+    s.chars().map(|_| 1).sum()
 }
