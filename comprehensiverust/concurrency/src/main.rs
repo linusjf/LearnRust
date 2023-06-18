6479338f2919e4a92bb6aad6c361865e18305442
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-18 10:41:00 +0530

    feat: add concurrency example

--- comprehensiverust/concurrency/src/main.rs
+++ comprehensiverust/concurrency/src/main.rs
@@ -1,3 +1,4 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
@@ -29,4 +30,15 @@ fn main() {
     }
     let res = handle.join();
     println!("{:#?}", res);
+    let (tx, rx) = mpsc::channel();
+
+    tx.send(10).unwrap();
+    tx.send(20).unwrap();
+
+    println!("Received: {:?}", rx.recv());
+    println!("Received: {:?}", rx.recv());
+
+    let tx2 = tx.clone();
+    tx2.send(30).unwrap();
+    println!("Received: {:?}", rx.recv());
 }
