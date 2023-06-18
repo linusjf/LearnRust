f227bc7cade4f66f209b4c29b558a931b9a7f166
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-18 12:59:43 +0530

    feat: add concurrency example

--- comprehensiverust/concurrency/src/main.rs
+++ comprehensiverust/concurrency/src/main.rs
@@ -1,4 +1,5 @@
 use std::sync::mpsc;
+use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
@@ -56,4 +57,31 @@ fn main() {
     for msg in rx.iter() {
         println!("Main: got {msg}");
     }
+    let (tx, rx) = mpsc::sync_channel(3);
+
+    thread::spawn(move || {
+        let thread_id = thread::current().id();
+        for i in 1..10 {
+            tx.send(format!("Message {i}")).unwrap();
+            println!("{thread_id:?}: sent Message {i}");
+        }
+        println!("{thread_id:?}: done");
+    });
+    thread::sleep(Duration::from_millis(100));
+
+    for msg in rx.iter() {
+        println!("Main: got {msg}");
+    }
+
+    let v = Arc::new(vec![10, 20, 30]);
+    let mut handles = Vec::new();
+    for _ in 1..5 {
+        let v = Arc::clone(&v);
+        handles.push(thread::spawn(move || {
+            let thread_id = thread::current().id();
+            println!("{thread_id:?}: {v:?}");
+        }));
+    }
+    handles.into_iter().for_each(|h| h.join().unwrap());
+    println!("v: {v:?}");
 }
