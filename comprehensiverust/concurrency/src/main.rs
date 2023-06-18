f52903d6582c105fe2c5cdaa00316b74ee721354
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-18 10:38:05 +0530

    feat: add concurrency example

--- comprehensiverust/concurrency/src/main.rs
+++ comprehensiverust/concurrency/src/main.rs
@@ -10,11 +10,23 @@ fn main() {
         }
     });
 
+    let s = String::from("Hello");
+
+    thread::spawn(move || {
+        println!("Length: {}", s.len());
+        thread::sleep(Duration::from_millis(5));
+    });
+    let s = String::from("Hello");
+
+    thread::scope(|scope| {
+        scope.spawn(|| {
+            println!("Length: {}", s.len());
+        });
+    });
     for i in 1..5 {
         println!("Main thread: {i}");
         thread::sleep(Duration::from_millis(5));
     }
-
     let res = handle.join();
     println!("{:#?}", res);
 }
