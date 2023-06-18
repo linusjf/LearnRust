2f78972cf1285217b29277d8327e21740a429c68
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-18 13:51:40 +0530

    feat: add concurrency example

--- comprehensiverust/concurrency/src/main.rs
+++ comprehensiverust/concurrency/src/main.rs
@@ -1,5 +1,6 @@
 use std::sync::mpsc;
 use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
 
@@ -75,7 +76,7 @@ fn main() {
 
     let v = Arc::new(vec![10, 20, 30]);
     let mut handles = Vec::new();
-    for _ in 1..5 {
+    for _ in 0..10 {
         let v = Arc::clone(&v);
         handles.push(thread::spawn(move || {
             let thread_id = thread::current().id();
@@ -84,4 +85,11 @@ fn main() {
     }
     handles.into_iter().for_each(|h| h.join().unwrap());
     println!("v: {v:?}");
+    let v = Mutex::new(vec![10, 20, 30]);
+    println!("v: {:?}", v.lock().unwrap());
+    {
+        let mut guard = v.lock().unwrap();
+        guard.push(40);
+    }
+    println!("v: {:?}", v.lock().unwrap());
 }
