7ce9627a25070704b59ab00e158af2a7fd5c95a7
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-19 17:26:49 +0530

    feat: update tokio example

--- comprehensiverust/concurrency/src/tokioexample.rs
+++ comprehensiverust/concurrency/src/tokioexample.rs
@@ -10,10 +10,12 @@ async fn count_to(count: i32) {
 
 #[tokio::main]
 pub async fn main() {
-    tokio::spawn(count_to(10));
+    let handle = tokio::spawn(count_to(10));
 
     for i in 1..5 {
         println!("Main task: {i}");
         time::sleep(time::Duration::from_millis(5)).await;
     }
+    let _res = handle.await;
+    count_to(10).await;
 }
